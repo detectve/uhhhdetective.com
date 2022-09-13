53d991248c052bbf707e997ce4d28cbf85974b16
detectve <103901336+detectve@users.noreply.github.com>
2022-09-13 17:48:50 +0100

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>uhhhdetective.xyz</title>
+</svelte:head>
+
+<div id = "main"></div>
+<div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div class="rounded overflow-hidden shadow-lg bg-[#302e2e]">
+        
+    </div>
+    </div>
