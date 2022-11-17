207617e334ffca0f8166e402461f84a2701ab09f
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-11-17 15:04:49 +0000

    fix: build errors on CF pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
-import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-		  postcss: true,
-		}),
-	  ],
-	kit: {
-		adapter: adapter()
-	}
+  kit: {adapter: adapter(),
+  },
+  preprocess: [preprocess({postcss: true,}),
+  ],
 };
 
 export default config;
