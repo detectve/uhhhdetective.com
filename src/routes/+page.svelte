e22c6c023fa39ec98ee13ca4cf0e32799d4777d9
uhhhdetective <uhhhdetective@protonmail.com>
2023-01-09 10:14:00 +0000

    feat: edit info and start to add posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,8 @@
         <h1 class="text-white text-2xl font-semibold mb-4">About:</h1>
         <p class="text-white">→ Hi, I'm Em!</p>
         <p class="text-white">→ I'm a 16 year old aspiring Front-End/Full Stack developer.</p>
-        <p class="text-white">→ I program random things as and when I feel</p>
         <p class="text-white">→ HTML, CSS, JavaScript, SvelteKit, Python</p>
-        <p class="text-white">→ I make websites, program bots for discord, and other stuff.</p>
+        <p class="text-white">→ I make websites, program bots for discord, and other projects for personal use.</p>
       </div>
     </div>
 
@@ -32,10 +31,10 @@
         <p class="text-white">→ A bot for Discord that upon command shows UK train times for a station of your choice, including delays and cancellations, using the Realtime Trains API.</p>
         <br />
         <h2 class="text-white text-xl font-semibold glowhover">Dot Matrix (personal project, under current development)</h2>
-        <p class="text-white">→ A dot matrix display that informs the user of arrival and departure time, delays and cancellations for a station of their choice, using the Realtime Trains API.</p>
+        <p class="text-white">→ A dot matrix display that informs the user of arrival and departure times of trains, delays and cancellations for a station of their choice, using the Realtime Trains API.</p>
         <br />
         <h2 class="text-white text-xl font-semibold glowhover">uhhhdetective.xyz, this website (work in progress)</h2>
-        <p class="text-white">→ The website you're on now, made with SvelteKit and Tailwind CSS, hosted by Cloudflare Pages.</p>
+        <p class="text-white">→ The website you're on now, made with SvelteKit and Tailwind CSS, hosted with Cloudflare Pages.</p>
       </div>
     </div>
 
@@ -43,21 +42,21 @@
       <div class="px-6 py-4 top-0 right-0">
         <h1 class="text-white text-2xl font-semibold mb-4">Contact:</h1>
         <p class="text-white glowhover">
+          <a href="https://github.com/detectve">
+            <i class="fab fa-github" /> detectve</a>
+        </p>
+        <p class="text-white glowhover">
           <a href="https://discord.com/users/718798976211484724">
             <i class="fab fa-discord " /> uhhhdetective#1337
           </a>
         </p>
         <p class="text-white glowhover">
-          <a href="https://twitter.com/uhhhdetective">
-            <i class="fab fa-twitter" /> @uhhhdetective</a>
-        </p>
-        <p class="text-white glowhover">
-          <a href="https://github.com/detectve">
-            <i class="fab fa-github" /> detectve</a>
-        </p>
-        <p class="text-white glowhover">
           <a href="mailto:uhhhdetective@protonmail.com">
             <i class="fas fa-envelope" /> uhhhdetective@protonmail.com</a>
+        </p>
+        <p class="text-white glowhover">
+          <a href="https://twitter.com/uhhhdetective">
+            <i class="fab fa-twitter" /> @uhhhdetective</a>
         </p>
       </div>
     </div>
---
+++ src/routes/posts/[id]/+page.svelte
@@ -0,0 +1,37 @@
+<style lang="postcss">
+    .highlight {
+      @apply rounded-md bg-[#2a2a2a] p-1 text-white;
+      font-family: "jetbrains mono", monospace;
+    }
+    .highlightUnderline {
+      @apply rounded-md bg-[#2a2a2a] p-1 text-white hover:underline;
+      font-family: "jetbrains mono", monospace;
+    }
+    .monofont {
+      font-family: "jetbrains mono", monospace !important;
+    }
+    </style>
+    
+    <script>
+    export let data;
+    </script>
+    
+    <svelte:head>
+      <title>{data.title} — dromzeh.dev</title>
+      <meta name="description" content="get real" />
+    </svelte:head>
+    
+    <div class="mx-auto max-w-screen-lg px-3 py-6">
+      <article
+        class="prose-table:th:text-blue-400 prose-table:th:hover:text-sky-100 prose-table:td:text-white prose-table:td:hover:text-sky-100 prose prose-slate max-w-7xl list-disc prose-headings:bg-gradient-to-br prose-headings:bg-clip-text prose-headings:font-semibold prose-headings:text-transparent prose-headings:text-white prose-p:text-gray-400 prose-a:text-white prose-blockquote:text-white prose-strong:text-white prose-em:underline prose-code:text-white prose-li:text-gray-400 prose-table:text-white"
+      >
+        <div class="p-3 md:p-10 lg:p-20">
+          <h4 class="monofont text-3xl text-white">{data.title}</h4>
+          <p class="text-lg text-white">
+            {data.description}
+            <span class="text-xs text-gray-400">(Published: {data.date})</span>
+          </p>
+          <svelte:component this="{data.content}" />
+        </div>
+      </article>
+    </div>
