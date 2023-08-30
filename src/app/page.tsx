c5e363ea272c48bbd9cdec0f2bf2c5246a7c4443
uhhhdetective <uhhhdetective@protonmail.com>
2023-08-30 22:00:37 +0100

    feat: add links to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
             </div>
             <p className="text-lg text-zinc-400">
                 I mostly work with Next, SvelteKit and TypeScript, but I also have
-                 experience with Python and C#. I am looking to expand this knowledge 
-                 and learn about more technologies in the near future.
+                experience with Python and C#. I am looking to expand this knowledge 
+                and learn about more technologies in the near future.
             </p>
         </div>
     )
